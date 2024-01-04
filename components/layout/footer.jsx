97aa2c9c18e0a2cceb698d3c853548db44e74c05
Mohammed R <mohammed.rmich@etu.univ-tours.fr>
2024-01-04 12:34:03 +0100

    footer

--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -1,51 +1,17 @@
 "use client";
 import React from "react";
-import { BsFillSuitHeartFill } from "react-icons/bs";
-import { Link } from "@nextui-org/react";
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
-import { links } from "@/content/socials";
+import { Card, CardBody } from "@nextui-org/react";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#dde3e9] shadow m-2 dark:bg-gray-800 absolute bottom-0 w-full">
-      <div
-        className="flex flex-col md:flex-row items-center justify-between w-full mx-auto max-w-screen-xl p-2
-            text-sm md:text-lg text-gray-500 dark:text-gray-400"
-      >
-        <span className="text-center">
-          © 2023 Lamia C.™ All Rights Reserved.
-        </span>
-
-        <span className="flex flex-row items-center gap-2">
-          Made with
-          <BsFillSuitHeartFill color="red" />
-          <span>by</span>
-          <Link href={links.hopymed} target="_blank" rel="noopener noreferrer">
-            HopyMed
-          </Link>
-        </span>
-      </div>
-
-      <div className="flex gap-4 flex-row justify-center md:justify-start w-full mx-auto max-w-screen-xl p-2">
-        <Link
-          href={links.github}
-          target="_blank"
-          color="foreground"
-          rel="noopener noreferrer"
-          aria-label="Github"
-        >
-          <AiFillGithub className="w-8 h-8" />
-        </Link>
-        <Link
-          href={links.linkedin}
-          target="_blank"
-          color="foreground"
-          rel="noopener noreferrer"
-          aria-label="Linkedin"
-        >
-          <AiFillLinkedin className="w-8 h-8" />
-        </Link>
-      </div>
+    <footer className="h-auto mt-20 md:mt-36 flex justify-center p-4">
+      <Card className="w-full rounded-none bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-100 via-transparent to-orange-100 shadow-2xl">
+        <CardBody>
+          <span className="text-center text-large">
+            © 2023 Lamia C.™ All Rights Reserved.
+          </span>
+        </CardBody>
+      </Card>
     </footer>
   );
 };
