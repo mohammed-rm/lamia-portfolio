01b2eee8f8c0875882ca43451fc165a399edb5b2
Mohammed R <mohammed.rmich@etu.univ-tours.fr>
2024-01-05 19:04:09 +0100

    projects sub category size problem

--- components/projects/project-category.jsx
+++ components/projects/project-category.jsx
@@ -4,11 +4,13 @@ const ProjectCategory = ({ title, subCategories }) => {
   return (
     <div className="w-full h-auto relative p-8">
       <div className="absolute inset-0 bg-gradient-to-br from-transparent to-blue-200 opacity-20 rounded-large rounded-br-[100px]" />
-      <p className="text-2xl ribbon h-auto">{title}</p>
+      <p className="text-sm md:text-large lg:text-xl ribbon h-auto font-semibold">
+        {title}
+      </p>
       <ul className="list-disc">
         {Object.values(subCategories).map((subCategory, index) => (
-          <li key={index} className="text-xl mt-10 md:ml-12">
-            {subCategory.title}
+          <li key={index} className="text-sm md:text-large md:ml-12 mt-14">
+            <span className="opacity-60">{subCategory.title}</span>
             {/*<div className="flex flex-wrap justify-center items-center gap-4 mt-6 -ml-6">*/}
             {/*  {subCategory.projects.map((project, index) => (*/}
             {/*    <ProjectCard*/}
