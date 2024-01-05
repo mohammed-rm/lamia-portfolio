b233aaa2f61ee2f3a9f0fd498d7486c42ceeb07a
Mohammed R <mohammed.rmich@etu.univ-tours.fr>
2024-01-05 14:21:35 +0100

    badges structure

--- content/badges.js
+++ content/badges.js
@@ -12,32 +12,7 @@ export const badges = {
     title:
       "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia; Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
     keywords: ["key1", "key2", "key3", "key4"],
-    badgeIcon: python,
-    link: "https://www.google.com",
-  },
-  3: {
-    title:
-      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia",
-    keywords: [
-      "key1",
-      "key2",
-      "key3",
-      "key4",
-      "key5",
-      "key6",
-      "key7",
-      "key8",
-      "key9",
-      "key10",
-    ],
     badgeIcon: sql,
-    link: "https://www.google.com",
-  },
-  4: {
-    title:
-      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia",
-    keywords: ["key1", "key2", "key3"],
-    badgeIcon: python,
     link: "https://www.google.com",
   },
 };
