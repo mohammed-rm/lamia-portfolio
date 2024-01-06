14db110986144447a428bbae4ea7a7a1255fe088
Mohammed R <mohammed.rmich@etu.univ-tours.fr>
2024-01-06 18:47:31 +0100

    hero animated divs

--- sections/hero.jsx
+++ sections/hero.jsx
@@ -4,13 +4,19 @@ import Image from "next/image";
 import heroImage from "@/public/hero-image.png";
 import { heroInfo } from "@/content/hero";
 import { useSectionInView } from "@/hooks/active-section-context";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   const { ref } = useSectionInView("hero");
   return (
     <section id="hero" className="min-h-screen md:pt-20 bg-green-50" ref={ref}>
       <div className="flex flex-col lg:flex-row items-center space-y-6 lg:space-y-0 lg:justify-evenly">
-        <div className="mt-24 flex flex-col space-y-6 items-center lg:items-start lg:w-2/5">
+        <motion.div
+          className="mt-24 flex flex-col space-y-6 items-center lg:items-start lg:w-2/5"
+          initial={{ x: -1000 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
+        >
           <span className="flex flex-col text-center lg:text-start text-4xl md:text-5xl lg:text-6xl font-bold">
             <p>
               {heroInfo.title}{" "}
@@ -20,15 +26,19 @@ const Hero = () => {
           <p className="p-6 text-center md:text-2xl tracking-wide md:tracking-widest lg:text-start lg:p-0">
             {heroInfo.description}
           </p>
-        </div>
-        <div>
+        </motion.div>
+        <motion.div
+          initial={{ x: 1000 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
+        >
           <Image
             src={heroImage}
             alt="Hero Image"
             className="w-64 h-64 md:w-96 md:h-96 lg:w-[400px] lg:h-[400px]"
             priority
           />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
